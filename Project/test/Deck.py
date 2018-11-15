8fd0cbf67621260bf94f725f5795de7f1dad14a3
2015180012박건호 <ehdrpakt@naver.com>
2018-11-15 19:41:05 +0900

    덱 셔플후 덱랜덤출력
    캐릭터마다 생성

--- Project/test/Deck.py
+++ Project/test/Deck.py
@@ -13,7 +13,6 @@ import game_world
 name = "deck"
 image = None
 next = None
-character = None
 Enemycharacter=None
 Enemycharacter=None
 mouse_x,mouse_y=0,0
@@ -41,25 +40,78 @@ class PlayDeck:
         shuffle()
         self.x, self.y = 100, 100
         self.ikuDeck= load_image('IkuSpellCard.png')
+        self.reimuDeck= load_image('RimuSpellCard.png')
+        self.marisaDeck= load_image('MarisaSpellCard.png')
+        self.tenshiDeck= load_image('TenshiSpellCard.png')
     def update(self):
         pass
 
     def draw(self):
-        for i in range(0,12):
-            if PlayerDeck[i]==1:
-                self.ikuDeck.clip_draw(0, 0, 45, 65, self.x+(50*i), self.y)
-            if PlayerDeck[i]==2:
-                self.ikuDeck.clip_draw(45, 0, 45, 65, self.x+(50*i), self.y)
-            if PlayerDeck[i]==3:
-                self.ikuDeck.clip_draw(90, 0, 45, 65, self.x+(50*i), self.y)
-            if PlayerDeck[i]==4:
-                self.ikuDeck.clip_draw(135, 0, 45, 65, self.x+(50*i), self.y)
-            if PlayerDeck[i]==5:
-                self.ikuDeck.clip_draw(0, 65, 45, 65, self.x+(50*i), self.y)
-            if PlayerDeck[i]==6:
-                self.ikuDeck.clip_draw(45, 65, 45, 65, self.x+(50*i), self.y)
-            if PlayerDeck[i]==7:
-                self.ikuDeck.clip_draw(90, 65, 45, 65, self.x+(50*i), self.y)
+        if CharacterSelection.character==0:
+            for i in range(0,12):
+                if PlayerDeck[i]==1:
+                    self.reimuDeck.clip_draw(0, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==2:
+                    self.reimuDeck.clip_draw(45, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==3:
+                    self.reimuDeck.clip_draw(90, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==4:
+                    self.reimuDeck.clip_draw(135, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==5:
+                    self.reimuDeck.clip_draw(0, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==6:
+                    self.reimuDeck.clip_draw(45, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==7:
+                    self.reimuDeck.clip_draw(90, 65, 45, 65, self.x+(50*i), self.y)
+        if CharacterSelection.character==1:
+            for i in range(0,12):
+                if PlayerDeck[i]==1:
+                    self.marisaDeck.clip_draw(0, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==2:
+                    self.marisaDeck.clip_draw(45, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==3:
+                    self.marisaDeck.clip_draw(90, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==4:
+                    self.marisaDeck.clip_draw(135, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==5:
+                    self.marisaDeck.clip_draw(0, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==6:
+                    self.marisaDeck.clip_draw(45, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==7:
+                    self.marisaDeck.clip_draw(90, 65, 45, 65, self.x+(50*i), self.y)
+        if CharacterSelection.character==2:
+            for i in range(0,12):
+                if PlayerDeck[i]==1:
+                    self.ikuDeck.clip_draw(0, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==2:
+                    self.ikuDeck.clip_draw(45, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==3:
+                    self.ikuDeck.clip_draw(90, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==4:
+                    self.ikuDeck.clip_draw(135, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==5:
+                    self.ikuDeck.clip_draw(0, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==6:
+                    self.ikuDeck.clip_draw(45, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==7:
+                    self.ikuDeck.clip_draw(90, 65, 45, 65, self.x+(50*i), self.y)
+
+        if CharacterSelection.character==3:
+            for i in range(0,12):
+                if PlayerDeck[i]==1:
+                    self.tenshiDeck.clip_draw(0, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==2:
+                    self.tenshiDeck.clip_draw(45, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==3:
+                    self.tenshiDeck.clip_draw(90, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==4:
+                    self.tenshiDeck.clip_draw(135, 0, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==5:
+                    self.tenshiDeck.clip_draw(0, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==6:
+                    self.tenshiDeck.clip_draw(45, 65, 45, 65, self.x+(50*i), self.y)
+                if PlayerDeck[i]==7:
+                    self.tenshiDeck.clip_draw(90, 65, 45, 65, self.x+(50*i), self.y)
 
 
     def handle_event(self, event):
