6ccb03fe6c6ffd5648aa5e187222246561465a96
2015180012박건호 <ehdrpakt@naver.com>
2018-11-22 08:55:29 +0900

    bgm 볼륨수점

--- Project/test/backgroundmusic.py
+++ Project/test/backgroundmusic.py
@@ -4,15 +4,15 @@ class BG_Music:
     def __init__(self):
         if BackgroundSelection.BGcheak==0:
             self.bgm = load_music('C:\\2DGP\\2015180012-2DGP-PROJECT\\2DGP-PROJECT\\Project\\FCGimage\\Background\\bamboo.mp3')
-            self.bgm.set_volume(30)
+            self.bgm.set_volume(25)
             self.bgm.repeat_play()
         if BackgroundSelection.BGcheak==1:
             self.bgm = load_music('C:\\2DGP\\2015180012-2DGP-PROJECT\\2DGP-PROJECT\\Project\\FCGimage\\Background\\shrine.mp3')
-            self.bgm.set_volume(30)
+            self.bgm.set_volume(25)
             self.bgm.repeat_play()
         if BackgroundSelection.BGcheak==2:
             self.bgm = load_music('C:\\2DGP\\2015180012-2DGP-PROJECT\\2DGP-PROJECT\\Project\\FCGimage\\Background\\clocktower.mp3')
-            self.bgm.set_volume(30)
+            self.bgm.set_volume(25)
             self.bgm.repeat_play()
 
     def update(self):
